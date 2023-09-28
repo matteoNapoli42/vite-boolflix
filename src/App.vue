35dc63674673ac4ed8b19f2a310ee8bd4ea09fac
matteoNapoli42 <matteon4242@gmail.com>
2023-09-28 16:20:12 +0200

    add input and button. Search films based on input

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,38 @@ export default {
   data() {
     return {
       state,
+      searchedFilm: "",
     }
+  },
 
+  methods: {
+    clearInput() {
+      this.searchedFilm = "";
+    }
   },
   created() {
-    state.searchFilms("star wars");
+    //state.searchFilms(this.takeInput(this.searchedFilm));
   }
 }
 
 </script>
 
-<template></template>
+<template>
+  <div class="container">
+    <!--SearchZone-->
+    <div class="d-flex justify-content-center gap-3 mx-auto py-4">
+      <input type="text" v-model="searchedFilm" placeholder="Cerca un film">
+      <button class="btn btn-primary" @click="state.searchFilms(searchedFilm), clearInput()">Cerca</button>
+    </div>
+    <!--SearchedElements-->
+    <div>
+      <h3>Titolo</h3>
+      <h5>Titolo Originale</h5>
+      <span>Lingua</span>
+      <span>Voto</span>
+    </div>
+
+  </div>
+</template>
 
 <style ></style>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './assets/scss/custom.scss'
 createApp(App).mount('#app')
